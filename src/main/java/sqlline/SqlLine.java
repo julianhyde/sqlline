9b332ecc54b45d8c03d2ca180784b7048f037e2f
joeposner <joe.posner@gmail.com>
2013-06-10 16:03:40 -0700

    properly register callback

--- src/main/java/sqlline/SqlLine.java
+++ src/main/java/sqlline/SqlLine.java
@@ -3819,6 +3819,7 @@ public class SqlLine
 
                     if (call) {
                         stmnt = con().connection.prepareCall(sql);
+                        callback.trackSqlQuery(stmnt);
                         hasResults = ((CallableStatement) stmnt).execute();
                     } else {
                         stmnt = createStatement();
