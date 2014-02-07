49ad6fefc72c53dba712f2d42ca8b79c64359ab8
julianhyde <julianhyde@gmail.com>
2014-02-07 01:27:19 -0800

    Fix checkstyle issues.

---
+++ src/main/java/sqlline/package-info.java
@@ -0,0 +1,18 @@
+/*
+// Licensed to Julian Hyde under one or more contributor license
+// agreements. See the NOTICE file distributed with this work for
+// additional information regarding copyright ownership.
+//
+// Julian Hyde licenses this file to you under the Modified BSD License
+// (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at:
+//
+// http://opensource.org/licenses/BSD-3-Clause
+*/
+
+/**
+ * SqlLine, a shell for issuing SQL to relational databases via JDBC.
+ */
+package sqlline;
+
+// End package-info.java
