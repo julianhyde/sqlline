c5d1f837f9b463cd557fba7db2ee8ff232d34072
snuyanzin <snuyanzin@gmail.com>
2018-11-02 16:32:48 +0300

    [SQLLINE-186] Added tests for completions. Added possibility to do compmletion for properties values like outputformat or boolean properties

--- src/main/java/sqlline/SqlLineCommandCompleter.java
+++ src/main/java/sqlline/SqlLineCommandCompleter.java
@@ -14,6 +14,7 @@ package sqlline;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.jline.builtins.Completers;
 import org.jline.reader.Completer;
 import org.jline.reader.impl.completer.AggregateCompleter;
 import org.jline.reader.impl.completer.ArgumentCompleter;
@@ -26,16 +27,22 @@ import org.jline.reader.impl.completer.StringsCompleter;
 class SqlLineCommandCompleter extends AggregateCompleter {
   SqlLineCommandCompleter(SqlLine sqlLine) {
     super(new LinkedList<>());
-    List<ArgumentCompleter> completers = new LinkedList<>();
+    List<Completer> completers = new LinkedList<>();
 
     for (CommandHandler commandHandler : sqlLine.getCommandHandlers()) {
       for (String cmd : commandHandler.getNames()) {
         List<Completer> compl = new LinkedList<>();
-        compl.add(new StringsCompleter(SqlLine.COMMAND_PREFIX + cmd));
-        compl.addAll(commandHandler.getParameterCompleters());
-        compl.add(new NullCompleter()); // last param no complete
-
-        completers.add(new ArgumentCompleter(compl));
+        if (!commandHandler.getParameterCompleters().isEmpty()
+            && commandHandler.getParameterCompleters().iterator().next()
+                instanceof Completers.RegexCompleter) {
+          completers.add(
+              commandHandler.getParameterCompleters().iterator().next());
+        } else {
+          compl.add(new StringsCompleter(SqlLine.COMMAND_PREFIX + cmd));
+          compl.addAll(commandHandler.getParameterCompleters());
+          compl.add(new NullCompleter()); // last param no complete
+          completers.add(new ArgumentCompleter(compl));
+        }
       }
     }
 
