22f49fe76a5df1b963200946c93efe67e97fd4b1
snuyanzin <snuyanzin@gmail.com>
2019-07-30 00:59:20 +0200

    [SQLLINE-331] Add comment for Dialect#getExtraNameCharacters

--- src/main/java/sqlline/Dialect.java
+++ src/main/java/sqlline/Dialect.java
@@ -45,6 +45,12 @@ interface Dialect {
 
   boolean isUpper();
 
+  /**
+   * Retrieves all the "extra" characters that can be used
+   * in unquoted identifier names (those beyond a-z, A-Z, 0-9 and _).
+   * 
+   * @return the string containing the extra characters
+   */
   String getExtraNameCharacters();
 }
 
